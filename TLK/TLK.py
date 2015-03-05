75eace0f8fe8c35a27ce1b6cde3bac4bad4817bf
SusInMotion <steinman.tutoring@gmail.com>
2015-03-05 13:59:32 -0500

    tag words redirects to pos confirm. NOT WORKING Yet sorry.

--- TLK/TLK.py
+++ TLK/TLK.py
@@ -15,8 +15,29 @@ app.config.from_object(__name__)
 def login():
 	return render_template("login.html")
 
+@app.route("/<username>")
+def show_sentences():
+	#this pulls any users from the database that match the credentials given. If there is one, it displays the sentences from this user.
+	#if not, it 
+	pass
 
-#import views
+def input_sentence():
+	pass
+
+
+@app.route("/tag")
+def tag_pos(sentence="Susan and Ian are making an app"):
+	words=sentence.split()
+	return render_template("tag_words.html", words=words)
+
+@app.route("/confirmPOS")
+@app.route("/tag/<word>")
+def pos_confirm(words):
+	print "hi"
+	for word in words:
+		print request.args.get("word")
+	return render_template("login.html")
 if __name__ == '__main__':
     app.run()
+
 
