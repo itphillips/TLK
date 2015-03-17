aa679db5e4a9b18202eae9103b013482e462cb6f
SusInMotion <steinman.tutoring@gmail.com>
2015-03-17 14:30:37 -0400

    some progress on login screen and tag words. Tag words not sending information to TLK.py properly

--- TLK/TLK.py
+++ TLK/TLK.py
@@ -10,17 +10,26 @@ from psycopg2 import extras
 app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
 
+conn = psycopg2.connect("postgres://pmehzpfkeotntn:u4OXp20HhAef8TD8L9Hqk1LciC@ec2-174-129-21-42.compute-1.amazonaws.com:5432/d6ki3e1ckkv6f3")
+
+
+dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
 @app.route("/")
 def login():
 	return render_template("login.html")
 
-@app.route("/<username>")
+@app.route("/show_sentences")
 def show_sentences():
 	#this pulls any users from the database that match the credentials given. If there is one, it displays the sentences from this user.
 	#if not, it 
-	pass
+	username=request.args.get("username")
+	print username, "username"
+	dict_cur.execute("SELECT * FROM sentences WHERE username = '{0}';".format( username ))
+	sentences=dict_cur.fetchall()
+	if sentences == []:
+		return redirect (url_for('input_sentence'))
 
 def input_sentence():
 	pass
@@ -32,12 +41,11 @@ def tag_pos(sentence="Susan and Ian are making an app"):
 	return render_template("tag_words.html", words=words)
 
 @app.route("/confirmPOS")
-@app.route("/tag/<word>")
-def pos_confirm(words):
-	print "hi"
-	for word in words:
-		print request.args.get("word")
-	return render_template("login.html")
+def pos_confirm():
+	words=request.args.get("words")
+	print words
+	return render_template("tag_words.html", words=words)
+
 if __name__ == '__main__':
     app.run()
 
