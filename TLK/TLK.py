bc2019c2139f7959f5f1221bbc5e21ddf1612cf6
SusInMotion <steinman.tutoring@gmail.com>
2015-05-01 15:49:17 -0400

    working through tagging POS. If you say you don't agree with the sentence, redirect not working

--- TLK/TLK.py
+++ TLK/TLK.py
@@ -10,8 +10,8 @@ from psycopg2 import extras
 app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
 
-conn = psycopg2.connect("postgres://pmehzpfkeotntn:u4OXp20HhAef8TD8L9Hqk1LciC@ec2-174-129-21-42.compute-1.amazonaws.com:5432/d6ki3e1ckkv6f3")
-
+#conn = psycopg2.connect("postgres://pmehzpfkeotntn:u4OXp20HhAef8TD8L9Hqk1LciC@ec2-174-129-21-42.compute-1.amazonaws.com:5432/d6ki3e1ckkv6f3")
+conn = psycopg2.connect("user=SusanSteinman")
 
 dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
@@ -26,7 +26,6 @@ def show_sentences():
 	#if not, it 
 	username=request.args.get("username")
 	password=request.args.get("password")
-
 	dict_cur.execute("SELECT id FROM users WHERE username = '{}' AND password= '{}';".format( username, password ))
 	
 	userID=dict_cur.fetchone()
@@ -48,36 +47,71 @@ def signup():
 	if request.method=="GET":
 		return render_template("signup.html", username=username, password=password)
 	else:
-		#this says it's working but secretly isn't (!)
-		dict_cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)",(username, password))
+		try:
+			dict_cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)",(username, password))
+		except Exception as e:
+			print e
 		return redirect(url_for('show_sentences', username=username, password=password))
 
 
 @app.route("/input")
 def input_sentence():
 	userID=request.args.get("userID")
+	print userID
 	return render_template("input_sentence.html", userID=userID)
 
 @app.route("/sentence")
 def confirm_sentence():
-	userID=request.args.get("userID")
-	sentence=request.args.get("sentence")
-	language=request.args.get("language")
-	date=request.args.get("date")
-
+	print "yoooo"
+	try:
+		userID=request.args.get("userID")
+		sentence=request.args.get("sentence")
+		language=request.args.get("language")
+		date=request.args.get("date")
+	except Exception as e:
+		print e
+	print userID
+	print sentence
+	print language
+	print date
+		#return redirect to same page with error message==make sure you fill in all fields.
 	#continued_session=request.args.get("continued_session")
 
-	sessionID=date+language
+
+
+	return render_template("confirm_sentence.html", userID=userID, sentence=sentence, language=language, date=date)
+
+@app.route("/tagPOS")
+def tag_pos():
+	print "yo"
+	try:
+		sentence = request.args.get("sentence")
+		userID = request.args.get("userID")
+		language=request.args.get("language")
+		date=request.args.get("date")
+		sessionID=date+language
+	except Exception as e:
+		print e
+
+	try:
+		print sentence
+	except Exception as e:
+		print e
+	print userID
+	print language
+	print date
+	print sessionID
+	
 	try:
 		dict_cur.execute("SELECT sessionnumber FROM sentences s INNER JOIN users_sentences us ON us.userID=s.ID WHERE us.userID = '{}'  AND sessionID='{}';".format(userID, sessionID))
 	except Exception as e:
 		print e
+	print dict_cur.fetchall()
 	if dict_cur.fetchall() != []:
-		#may want to check if the sentence is identical or not...
 		sessionnumber=sessionnumber+1
 	else:
 		sessionnumber=1	
-	
+	print sessionnumber
 	try:
 		dict_cur.execute("INSERT INTO sentences (sentence, language, collection_date, sessionnumber, sessionID) VALUES (%s,%s, %s,%s, %s)",(sentence, language, date, sessionnumber, sessionID))
 		dict_cur.execute("SELECT id FROM sentences WHERE sentence = '{}' AND collection_date = '{}' AND sessionID = '{}';".format (sentence, date, sessionID) )
@@ -86,28 +120,20 @@ def confirm_sentence():
 	except Exception as e:
 		print e	
 
-	return render_template("confirm_sentence.html", sentence=sentence, sentenceID=sentenceID, userID=userID)
-	#this template lets the user see if they like the sentence. If they don't, it redirects to input sentence. If they do, it directs to tag_pos
-
-@app.route("/tagPOS")
-def tag_pos():
-	sentence = request.args.get(sentence)
-	userID = request.args.get(userID)
-	sentenceID = request.args.get(sentenceID)
 	return render_template("tag_words.html", sentence=sentence, userID=userID, sentenceID=sentenceID)
 
 @app.route("/confirmPOS")
 def pos_confirm():
 	sentence=request.args.get("sentence")
-	userID = request.args.get(userID)
-	sentenceID = request.args.get(sentenceID)
+	userID = request.args.get("userID")
+	sentenceID = request.args.get("sentenceID")
 	return render_template("POS_confirm.html", sentence=sentence, userID=userID, sentenceID=sentenceID)
 
 @app.route("/")
 def pos_confirm_redirect():
 	if request.args.get("YES!"):
 		#THIS IS WHERE THIS STOPS WORKING!
-		return render_template ("")
+		return render_template ("group.html")
 	else:
 		return redirect(url_for("tag_pos")) 
 
