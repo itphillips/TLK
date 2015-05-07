18e53835a794bc1f5448b620f53e3365b839cd49
SusInMotion <steinman.tutoring@gmail.com>
2015-05-06 21:39:55 -0400

    fix database for pos. Also lots of print statements to make sure errors are printed

--- TLK/TLK.py
+++ TLK/TLK.py
@@ -31,6 +31,7 @@ def show_sentences():
 	
 	userID=dict_cur.fetchone()
 	if userID == None:
+		print "done show sentences"
 		return redirect (url_for('signup', username=username, password=password))
 	else:
 		try:
@@ -38,6 +39,7 @@ def show_sentences():
 			sentences=dict_cur.fetchall()
 		except Exception as e:
 			print e
+		print "done show sentences"
 		return render_template("show_sentences.html", sentences=sentences, userID= userID[0])
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -46,12 +48,14 @@ def signup():
 	password=request.args.get("password")
 
 	if request.method=="GET":
+		print "done signup"
 		return render_template("signup.html", username=username, password=password)
 	else:
 		try:
 			dict_cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)",(username, password))
 		except Exception as e:
 			print e
+		print "done signup"
 		return redirect(url_for('show_sentences', username=username, password=password))
 
 
@@ -62,7 +66,6 @@ def input_sentence():
 
 @app.route("/sentence")
 def confirm_sentence():
-	print "yoooo"
 	try:
 		userID=request.args.get("userID")
 		sentence=request.args.get("sentence")
@@ -70,13 +73,9 @@ def confirm_sentence():
 		date=request.args.get("date")
 	except Exception as e:
 		print e
-	print userID
-	print sentence
-	print language
-	print date
 		#return redirect to same page with error message==make sure you fill in all fields.
 	#continued_session=request.args.get("continued_session")
-
+	print "done confirm_sentence"
 	return render_template("confirm_sentence.html", userID=userID, sentence=sentence, language=language, date=date)
 
 @app.route("/tagPOS")
@@ -95,6 +94,7 @@ def tag_pos():
 		sessionID=date+language
 	except Exception as e:
 		print e
+	print sessionID
 
 	try:
 		dict_cur.execute("SELECT sessionnumber FROM sentences s INNER JOIN users_sentences us ON us.userID=s.ID WHERE us.userID = '{}'  AND sessionID='{}';".format(userID, sessionID))
@@ -113,7 +113,7 @@ def tag_pos():
 		dict_cur.execute("INSERT INTO users_sentences (userID, sentenceID) VALUES (%s, %s)", (userID, sentenceID))
 	except Exception as e:
 		print e	
-
+	print "all done tag pos"
 	return render_template("tag_words2.html", sentence=sentence, userID=userID, sentenceID=sentenceID, error=error, language=language)
 
 @app.route("/confirmPOS")
@@ -122,17 +122,17 @@ def pos_confirm():
 	userID = request.args.get("userID")
 	sentenceID = request.args.get("sentenceID")
 	language = request.args.get("language")
-	pos = []
+	pos = ""
 	for i in range(len(sentence.split())):
 		if request.args.get(str(i)):
-			print request.args.get(str(i))
 			try:
-				pos.append(request.args.get(str(i)))
+				pos=pos+(request.args.get(str(i)))+" "
 			except Exception as e:
 				print e
 		else:
+			print "error"
 			return redirect(url_for('tag_pos', sentence=sentence, userID=userID, sentenceID=sentenceID, error= 1, language=language))
-	print pos
+	print "done pos confirm"
 	return render_template("POS_confirm.html", sentence=sentence, userID=userID, sentenceID=sentenceID, pos=pos, language=language)
 
 @app.route("/group")
@@ -141,24 +141,23 @@ def group():
 	userID = request.args.get("userID")
 	sentenceID = request.args.get("sentenceID")
 	sentence = request.args.get("sentence")
-	pos = request.args.get("pos")
+	pos_array = request.args.get("pos").split()
+	print pos_array
 	words = sentence.split()
 	for i in range(len(words)):
-		print words[i]
-		print pos[i]
 		try:
-			dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos[i], language))
+			dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos_array[i], language))
 
 			if dict_cur.fetchall() == []:
-				dict_cur.execute("INSERT INTO words (word, pos, language) VALUES (%s, %s, %s)", (words[i], pos[i], language))
-				dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos[i], language))
+				dict_cur.execute("INSERT INTO words (word, pos, language) VALUES (%s, %s, %s)", (words[i], pos_array[i], language))
+				dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos_array[i], language))
 
 			wordID = dict_cur.fetchone()[0]
 
 			dict_cur.execute("INSERT INTO words_sentences (wordID, sentenceID) VALUES (%s, %s)", (wordID, sentenceID))
 		except Exception as e:
 			print e
-
+	print "hallo"
 	return render_template("group.html", sentence=sentence, userID=userID)
 
 @app.route("/record")
