cee8fa20929daf2e644e38f7f6510a605cbb77f2
SusInMotion <steinman.tutoring@gmail.com>
2015-05-08 22:26:13 -0400

    database and redo/back fixes. some repetition in sentences, users_sentences, phrases, phrases_sentences, words_phrases. Others ok. Still no ask about/tag object

--- TLK/TLK.py
+++ TLK/TLK.py
@@ -11,8 +11,8 @@ from psycopg2 import extras
 app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
 
-conn = psycopg2.connect("postgres://pmehzpfkeotntn:u4OXp20HhAef8TD8L9Hqk1LciC@ec2-174-129-21-42.compute-1.amazonaws.com:5432/d6ki3e1ckkv6f3")
-#conn = psycopg2.connect("user=SusanSteinman") 
+#conn = psycopg2.connect("postgres://pmehzpfkeotntn:u4OXp20HhAef8TD8L9Hqk1LciC@ec2-174-129-21-42.compute-1.amazonaws.com:5432/d6ki3e1ckkv6f3")
+conn = psycopg2.connect("user=SusanSteinman") 
 conn.set_session(autocommit=True)
 dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
@@ -31,7 +31,10 @@ def login():
 	print dict_cur.fetchall()
 	dict_cur.execute("SELECT * FROM words;")
 	print dict_cur.fetchall()
-	dict_cur.execute("SELECT * FROM words_sentences;")
+	try:
+		dict_cur.execute("SELECT * FROM words_sentences;")
+	except Exception as e:
+		print e
 	print dict_cur.fetchall()
 	return render_template("login.html")
 
@@ -119,9 +122,11 @@ def tag_pos():
 	else:
 		sessionnumber=1	
 	try:
+		#make sure that this sentence isn't already in the database
 		dict_cur.execute("INSERT INTO sentences (sentence, language, collection_date, sessionnumber, sessionID) VALUES (%s,%s, %s,%s, %s)",(sentence, language, date, sessionnumber, sessionID))
 		dict_cur.execute("SELECT id FROM sentences WHERE sentence = '{}' AND collection_date = '{}' AND sessionID = '{}';".format (sentence, date, sessionID) )
 		sentenceID= dict_cur.fetchone()[0]
+		#make sure that this sentence isn't already in the database users-sentennces
 		dict_cur.execute("INSERT INTO users_sentences (userID, sentenceID) VALUES (%s, %s)", (userID, sentenceID))
 	except Exception as e:
 		print e	
@@ -149,24 +154,34 @@ def pos_confirm():
 
 @app.route("/group")
 def group():
-	language=request.args.get("language")
+	print request.args
+	redo = request.args.get("redo")
+	print redo, "redo"
 	userID = request.args.get("userID")
 	sentenceID = request.args.get("sentenceID")
 	sentence = request.args.get("sentence")
-	pos_array = str(request.args.get("pos")).split()
-	words = sentence.split()
-	for i in range(len(words)):
-		try:
-			dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos_array[i], language))
-			found = dict_cur.fetchall()
-			if found == []:
-				dict_cur.execute("INSERT INTO words (word, pos, language) VALUES (%s, %s, %s)", (words[i], pos_array[i], language))
+
+	if redo == None:
+		language=request.args.get("language")
+		pos_array = str(request.args.get("pos")).split()
+		words = sentence.split()
+		for i in range(len(words)):
+			try:
 				dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos_array[i], language))
-				found = dict_cur.fetchall()
-			wordID = found[0]["id"]
-			dict_cur.execute("INSERT INTO words_sentences (wordID, sentenceID, position) VALUES (%s, %s, %s)", (wordID, sentenceID, i))
-		except Exception as e:
-			print e
+				found_words = dict_cur.fetchall()
+				print found_words, "found words"
+				if found_words == []:
+					dict_cur.execute("INSERT INTO words (word, pos, language) VALUES (%s, %s, %s)", (words[i], pos_array[i], language))
+					dict_cur.execute("SELECT id from words WHERE word = '{}' AND pos = '{}' AND language = '{}';".format(words[i], pos_array[i], language))
+					found_words = dict_cur.fetchall()
+				wordID = found_words[0]["id"]
+				dict_cur.execute("SELECT id from words_sentences WHERE wordID = '{}' AND sentenceID = '{}';".format(wordID, sentenceID))
+				found_words_sentences=dict_cur.fetchall()
+				if found_words_sentences == []:
+					dict_cur.execute("INSERT INTO words_sentences (wordID, sentenceID, position) VALUES (%s, %s, %s)", (wordID, sentenceID, i))
+			except Exception as e:
+				print e
+
 	print "hallo"
 	return render_template("group.html", sentence=sentence, userID=userID, sentenceID=sentenceID)
 
@@ -177,16 +192,28 @@ def tag_phrase_structure():
 	words=sentence.split()
 	userID = request.args.get("userID")
 	sentenceID=request.args.get("sentenceID")
-	print sentenceID
-	word_positions_in_phrase = request.args.get("phrase").split()
-	phrase=" ".join([str(words[int(word_position)]) for word_position in word_positions_in_phrase])
+	print sentenceID, "sentenceID"
+	redo = request.args.get("redo")
+	print redo
+	if redo=="True":
+		print "redo true"
+		word_positions_in_phrase_string= request.args.get("word_positions_in_phrase")
+	else:
+		word_positions_in_phrase_string = request.args.get("phrase")
+		print word_positions_in_phrase_string, "phrase"
 
+	word_positions_in_phrase = word_positions_in_phrase_string.split()
+	print word_positions_in_phrase, "word_positions_in_phrase"
+	
+	phrase=" ".join([str(words[int(word_position)]) for word_position in word_positions_in_phrase])
+	print phrase, "phrase"
 	phrase_type = request.args.get("phrase_type")
+	print phrase_type
 	phrase_type_dict = {"S":[("NP", "necessary"), ("VP", "necessary")], "NP": [("det","optional"), ("AP", "optional"), ("N","necessary"), ("PP","optional")], "VP":[("V","necessary"), ("PP","optional"), ("NP","optional"), ("NP2", "optional"), ("S","optional"), ("CP", "optional"), ("AP","optional"), ("PP2","optional")], "PP":[("P","necessary"), ("NP","optional") ,("PP","optional")], "AP":[("deg","optional"), ("A","necessary")], "CP": [("C","necessary"), ("S","necessary")] }
 
 	phrase_structure_options=phrase_type_dict[phrase_type]
-	print phrase_structure_options
-	return render_template("tag_phrase_structure.html", sentence=sentence, userID=userID, phrase=phrase, phrase_type=phrase_type, phrase_structure_options=phrase_structure_options, sentenceID=sentenceID)
+	print phrase_structure_options, "phrase_structure_options"
+	return render_template("tag_phrase_structure.html", sentence=sentence, userID=userID, phrase=phrase, phrase_type=phrase_type, phrase_structure_options=phrase_structure_options, sentenceID=sentenceID, word_positions_in_phrase=word_positions_in_phrase_string)
 
 @app.route("/confirm_phrase")
 def confirm_phrase():
@@ -196,32 +223,71 @@ def confirm_phrase():
 	userID = request.args.get("userID")
 	phrase_type = request.args.get("phrase_type")
 	phrase=request.args.get("phrase")
-	print phrase
-	print phrase_structure
-	return render_template("confirm_phrase.html", sentence=sentence, userID=userID, phrase=phrase, phrase_type=phrase_type, phrase_structure=phrase_structure, sentenceID=sentenceID)
+	word_positions_in_phrase=request.args.get("word_positions_in_phrase")
+	print word_positions_in_phrase, "word_positions_in_phrase"
+	print phrase, "phrase"
+	print phrase_structure, "phrase_structure"
+	return render_template("confirm_phrase.html", sentence=sentence, userID=userID, phrase=phrase, phrase_type=phrase_type, phrase_structure=phrase_structure, sentenceID=sentenceID, word_positions_in_phrase=word_positions_in_phrase)
 
+@app.route("/phr_to_database")
 def put_phrase_in_database():
+	print "hi"
 	phrase_structure=request.args.get("phrase_structure")
 	sentence= request.args.get("sentence")
 	sentenceID=request.args.get("sentenceID")
 	userID = request.args.get("userID")
 	phrase_type = request.args.get("phrase_type")
 	phrase=request.args.get("phrase")
-
+	word_positions_in_phrase=request.args.get("word_positions_in_phrase")
+	print word_positions_in_phrase
+	print "prh in database "
+	#should check if this phrase is already in the database and notify user
 	try: 
 		dict_cur.execute("INSERT INTO phrases (phrase, phrase_type, phrase_subtype) VALUES (%s, %s, %s)", (phrase, phrase_type, phrase_structure))
 	except Exception as e:
 		print e
+	print "insert phrases"
+
 	try:
 		dict_cur.execute("SELECT id from phrases WHERE phrase = '{}' AND phrase_type = '{}' AND phrase_subtype = '{}';".format(phrase, phrase_type, phrase_structure))
 	except Exception as e:
 		print e
+	print "select from phrases"
 	phraseID=dict_cur.fetchall()[0][0]
+
+	#should also check to see if this is in phrases_sentences
 	try:
 		dict_cur.execute("INSERT INTO phrases_sentences (phraseID, sentenceID) VALUES (%s, %s)", (phraseID, sentenceID))
 	except Exception as e:
 		print e
-	return redirect(url_for('group'))
+	print "insert into phrases_sentences"
+
+	try:
+		dict_cur.execute("SELECT position,wordID from words_sentences WHERE sentenceID = '{}';".format(sentenceID))
+		wordIDPairs= dict_cur.fetchall()
+	except Exception as e:
+		print e
+	print "select id pairs"
+	wordIDPairs.sort(key=lambda x: int(x[1]))
+	print wordIDPairs,"wordIDPairs"
+	wordIDs=[wordID[1] for wordID in wordIDPairs]
+	print wordIDs, "wordIDs"
+	print word_positions_in_phrase, "word_positions_in_phrase"
+	print type(word_positions_in_phrase)
+	word_positions_in_phrase=word_positions_in_phrase.split()
+	try:
+		word_ids_in_phrase = [wordIDs[int(position)] for position in word_positions_in_phrase]
+	except Exception as e:
+		print e
+	print word_ids_in_phrase, "word_ids_in_phrase"
+	#anddd check to see if they're in words_[hrases. They probably are if they're in one of the others...need to reason about this]
+	for i in range(len(word_ids_in_phrase)):
+		try:
+			dict_cur.execute("INSERT INTO words_phrases (wordID, phraseID, position) VALUES (%s, %s, %s)", (wordIDs[i], phraseID, i))
+		except Exception as e:
+			print e
+	print "hi"
+	return redirect(url_for('group', redo=False, sentence=sentence, sentenceID=sentenceID, userID=userID))
 
 @app.route("/tagSubj")
 def tag_subj():
